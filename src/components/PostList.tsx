1c25c625470ac52f963412c16adba9ff526c4f65
Ikenna Benedict Nwaka <inwaka39@gmail.com>
2025-08-13 20:55:37 +0200

    list

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -13,7 +13,6 @@ export default function PostList({ posts }: PostListProps) {
       </div>
     );
   }
-
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {posts.map((post) => (
