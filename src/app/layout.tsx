64c04d8d5e47cf7a3f424d99d8181d378161b103
Ikenna Benedict Nwaka <inwaka39@gmail.com>
2025-08-16 20:23:46 +0200

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+         <link rel="icon" href="/favicon.ico" />
+         <title>MyApp</title>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
