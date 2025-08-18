8cb59e585bcffa3869abbd0ed4bfe969b1862a4d
Ikenna Benedict Nwaka <inwaka39@gmail.com>
2025-08-18 21:17:47 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
     <div className="min-h-screen bg-gray-50">
       <Header />
      <GoBackButton />   
-
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="text-center mb-10 mt-6">
           <h1 className="text-4xl font-bold text-gray-900 mb-4">
