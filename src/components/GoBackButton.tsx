0d03074d769329823239c5554d414a10e2790cdd
Ikenna Benedict Nwaka <inwaka39@gmail.com>
2025-08-19 22:07:24 +0200

    button

--- src/components/GoBackButton.tsx
+++ src/components/GoBackButton.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 
 export default function GoBackButton() {
   const router = useRouter();
+  
   return (
     <button
       onClick={() => router.back()}
